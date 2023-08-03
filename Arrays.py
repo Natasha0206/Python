74587ed6d59fb53beea8fafa7f5e5135dc3a7178
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-08-03 19:42:40 +0530

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,8 @@
+n, m = map(int,input().split())
+for i in range(n//2):
+    j = int((2*i)+1)
+    print(('.|.'*j).center(m, '-'))
+print('WELCOME'.center(m,'-'))
+for i in reversed(range(n//2)):
+    j = int((2*i)+1)
+    print(('.|.'*j).center(m, '-'))
