bd107d049e0c58c9b89581c699a951f39f9a0aa2
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-08-29 01:06:31 +0530

    Add files via upload

--- Athlete_Sort.py
+++ Athlete_Sort.py
@@ -1,8 +1,13 @@
+# initiailzing map function
 N, M = map(int, input().split())
 
+# taking for rows
 rows = [input() for _ in range(N)]
 
+# taking input from user
 K = int(input())
 
+
+# sorting using sorted function
 for row in sorted(rows, key=lambda row: int(row.split()[K])):
     print(row)
