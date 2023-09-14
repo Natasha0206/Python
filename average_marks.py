7fc51571de37a57332cec29ed64edbd59e242695
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-09-14 11:13:06 +0530

    Add files via upload

---
+++ average_marks.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the marks in Sub1:"))
+b=int(input("Enter the marks in Sub2:"))
+c=int(input("Enter the marks in Sub3:"))
+d=int(input("Enter the marks in Sub4:"))
+e=int(input("Enter the marks in Sub5:"))
+avg=a+b+c+d+e/5
+print(avg)
