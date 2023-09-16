d5a379a7bd7f0878979992a7c6d63503784d7751
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-09-17 00:37:02 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,9 @@
+A = int(input("Enter the temp for Monday:"))
+B = int(input("Enter the temp for Tuesday:"))
+C = int(input("Enter the temp for Wednesday:"))
+D = int(input("Enter the temp for Thursday:"))
+E = int(input("Enter the temp for Friday:"))
+F = int(input("Enter the temp for Satday:"))
+G = int(input("Enter the temp for Sunday:"))
+F = (A+B+C+D+E+F+G)/7
+print(F)
