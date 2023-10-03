d6d41e18c8057a77dfd906608b9b2e46cae1aa89
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-10-03 23:59:59 +0530

    Add files via upload

---
+++ Mean_var_std.py
@@ -0,0 +1,6 @@
+import numpy
+N, M = map(int, input().split())
+A = numpy.array([list(map(int, input().split())) for n in range(N)])
+print(numpy.mean(A, axis = 1))
+print(numpy.var(A, axis = 0))
+print(numpy.round(numpy.std(A), 11))
