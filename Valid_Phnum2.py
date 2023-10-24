77e0cfec56e485dc4c3fe68fdb0d06e160059059
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-10-24 20:41:46 +0530

    Add files via upload

---
+++ Valid_Phnum2.py
@@ -0,0 +1,12 @@
+s = input("Enter the string")
+print("The original string is: ",s)
+int(i) 
+for i in s.split():
+    if i.isdigit():
+        print("The numbers list is :",str(i))
+        print(sum(d))
+if i!=int:
+    print("The string has no digits")
+
+
+
