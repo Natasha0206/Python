9eafb7baac9cfafc5b16c17a958926f795015020
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-12-19 11:56:47 +0530

    Add files via upload

---
+++ Truck Tour.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'truckTour' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY petrolpumps as parameter.
+#
+
+def truckTour(petrolpumps):
+    if __name__ == '__main__':
+        fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    petrolpumps = []
+
+    for _ in range(n):
+        petrolpumps.append(list(map(int, input().rstrip().split())))
+
+    result = truckTour(petrolpumps)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
